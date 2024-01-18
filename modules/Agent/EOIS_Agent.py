22a366a80424bfb632cdbf5babd412aa42daa013
lwx0218 <linzhen.luo@gmail.com>
2024-01-18 15:29:57 +0800

    Project landscape

---
+++ modules/Agent/EOIS_Agent.py
@@ -0,0 +1,16 @@
+#TODO 集成BaseAgent的初始化及基础功能，开发具体场景的子Agent
+
+from modules.Agent import BaseAgent
+
+
+class EOIS_agent:
+
+    def __init__(self):
+
+    def function(self):
+
+
+
+if __name__=='__main__':
+    eAgent = EOIS_agent
+
